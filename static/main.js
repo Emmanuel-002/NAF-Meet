781b8f9fc12f6ae2fbc73b1001c3dbf16b55db85
Emmanuel-002 <segseyman8@gmail.com>
2025-07-26 15:15:16 +0100

    first commit

--- static/main.js
+++ static/main.js
@@ -15,9 +15,9 @@ window.onload = () => { // When Window load
 
 var peer = new Peer(undefined, { // Now with our peer server up an running, let's connect our client peer js to the server
     // path: "https://naf-meet.onrender.com/peerjs",
-    host: "https://naf-meet.onrender.com",
+    // host: "https://naf-meet.onrender.com",
     path: "/peerjs",
-    // host: "/",
+    host: "/",
     port: "443",
     // secure: true
 });
