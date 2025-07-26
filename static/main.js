c117bb577d35a1292a5f22a53d6a1070d1ed559f
Emmanuel-002 <segseyman8@gmail.com>
2025-07-26 14:21:36 +0100

    first commit

--- static/main.js
+++ static/main.js
@@ -16,7 +16,7 @@ window.onload = () => { // When Window load
 var peer = new Peer(undefined, { // Now with our peer server up an running, let's connect our client peer js to the server
     path: "/peerjs",
     host: "/",
-    port: "10000",
+    port: "443",
     secure: true
 });
 
